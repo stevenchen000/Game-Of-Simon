4a3de04b89985fad3e1e07643c2dda0eb0ef231a
stevenchen000 <10522779+stevenchen000@users.noreply.github.com>
2023-05-21 18:58:20 -0700

    Game Over Screen Added
    Game over screen added and semi-functional
    
    Will appear a few seconds after you get a game over.
    Calls a restart event to reset the game state (hooking up functions pending)
    Will disappear when restarting

--- Assets/Scripts/Game Over UI/GameOverUI.cs
+++ Assets/Scripts/Game Over UI/GameOverUI.cs
@@ -2,6 +2,7 @@ using SOEventSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum GameOverUIState
 {
@@ -16,61 +17,93 @@ public class GameOverUI : MonoBehaviour
 
     private GameOverUIState uiState = GameOverUIState.Inactive;
     [SerializeField] private RectTransform gameOverUI;
-    [SerializeField] private Vector2 hiddenOffset;
+    [SerializeField] private CanvasGroup background;
+
+    [Space]
+    [Range(1, 5)]
+    [SerializeField] private float gameOverStartTime = 2f;
+    [Range(1,5)]
+    [SerializeField] private float transitionTime = 1f;
+
+    [SerializeField] private float activeY = 245;
+    [SerializeField] private float inactiveY = -131;
+
+    [SerializeField] private EventSO onGameRestart;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        uiState = GameOverUIState.Appearing;
-        gameOverUI.anchoredPosition = hiddenOffset;
+        Vector3 currPos = gameOverUI.transform.position;
+        gameOverUI.transform.position = new Vector3(currPos.x, inactiveY, currPos.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (uiState)
-        {
-            case GameOverUIState.Appearing:
-                UIAppear();
-                break;
-            case GameOverUIState.Active:
-                break;
-            case GameOverUIState.Disappearing:
-                UIDisappear();
-                break;
-            case GameOverUIState.Inactive:
-                break;
-        }
+        
     }
+
+    /****************
+     * Public
+     * ***************/
+
+    public void StartUIAppear()
+    {
+        StartChangeStateAfterTime(gameOverStartTime, GameOverUIState.Appearing);
+    }
+
+    public void StartUIDisappear()
+    {
+        ChangeState(GameOverUIState.Disappearing);
+    }
+
+    /********************
+     * Helpers
+     * ****************/
 
     private void UIAppear()
     {
-        float currY = gameOverUI.anchoredPosition.y;
-        float goalY = 0;
-        float newValue = LeanTween.easeInCubic(currY, goalY, 0.2f);
-        gameOverUI.anchoredPosition = new Vector2(0, newValue);
+        gameOverUI.transform.LeanMoveY(activeY, transitionTime).setEaseInBounce();
+        background.LeanAlpha(1, transitionTime);
+        background.blocksRaycasts = true;
+        StartChangeStateAfterTime(transitionTime, GameOverUIState.Active);
     }
 
     private void UIDisappear()
     {
+        gameOverUI.transform.LeanMoveY(inactiveY, transitionTime).setEaseOutBack();
+        background.LeanAlpha(0, transitionTime);
+        background.blocksRaycasts = false;
+        StartChangeStateAfterTime(transitionTime, GameOverUIState.Inactive);
 
-    }
-
-    private bool TweenUI(float stop, float leeway)
-    {
-        float currY = gameOverUI.anchoredPosition.y;
-        
-        return false;
+        onGameRestart.CallEvent();
     }
 
     private void ChangeState(GameOverUIState newState)
     {
-        switch (uiState)
+        switch (newState)
         {
-
+            case GameOverUIState.Appearing:
+                UIAppear();
+                break;
+            case GameOverUIState.Disappearing:
+                UIDisappear();
+                break;
         }
 
 
+    }
+
+    private void StartChangeStateAfterTime(float time, GameOverUIState newState)
+    {
+        StartCoroutine(ChangeStateAfterTime(time, newState));
+    }
+
+    private IEnumerator ChangeStateAfterTime(float time, GameOverUIState newState)
+    {
+        yield return new WaitForSecondsRealtime(time);
+        ChangeState(newState);
     }
 
 }
